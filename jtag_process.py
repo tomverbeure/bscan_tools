64872bedbf0bb893ec11c800cda98ff51f3ee2ba
Tom Verbeure <hombre+github@gmail.com>
2020-07-18 00:50:33 -0700

    Extract key presses sent to FPGA.

--- jtag_process.py
+++ jtag_process.py
@@ -48,6 +48,11 @@ PAUSE_IR            = 13
 EXIT2_IR            = 14
 UPDATE_IR           = 15
 
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
 def convert_tdi_tdo_str(str):
 
     # Short format: 0x155
@@ -116,13 +121,11 @@ def read_transactions(jtag_filename):
 
             if tdi != "": 
                 tdi = convert_tdi_tdo_str(tdi)
-                tdi = hex(tdi)
             else:
                 tdi = None
 
             if tdo != "": 
                 tdo = convert_tdi_tdo_str(tdo)
-                tdo = hex(tdo)
             else:
                 tdo = None
 
@@ -145,8 +148,54 @@ def read_transactions(jtag_filename):
 
 transactions = read_transactions(argv[0])
 
-pp.pprint(transactions)
+#pp.pprint(transactions)
 
+def match_bit_pattern(pattern, pattern_len, sequence, sequence_len, start_bit):
+
+    for first_bit in range(start_bit, sequence_len-pattern_len):
+        pattern_shifted = pattern << first_bit
+        sequence_masked = sequence & (((1<<pattern_len)-1) << first_bit)
+
+        if pattern_shifted == sequence_masked:
+            return first_bit
+
+    return -1
+
+def reverse_int(value, width):
+    b = '{:0{width}b}'.format(value, width=width)
+    return int(b[::-1], 2)
+
+special_list = []
+for num, transaction in enumerate(transactions):
+    if None and transaction["tap_state_nr"] in [SHIFT_DR, SHIFT_IR]:
+        bit_nr = match_bit_pattern(ord('e'), 8, transaction["tdi"], transaction["tdi_bits"], 0)
+        #bit_nr = match_bit_pattern(0xc0f, 12, transaction["tdi"], transaction["tdi_bits"], 0)
+        if bit_nr >= 0:
+            print("Transaction nr %d: bit_nr: %d:" % (num, bit_nr))
+            pp.pprint(transaction)
+            print("0x%x" % transaction["tdi"])
+            sys.exit()
+        
+
+    if True and transaction["tap_state_nr"] in [SHIFT_DR] and transaction["tdi_bits"] == 643:
+        special_list.append(transaction)
+
+
+diff = 0
+
+for g_num, g in enumerate(chunks(special_list, 8)):
+    print("============================================================")
+    print("Group %d:" % g_num)
+
+    for n in range(0,8):
+        #print("%d: %x" % (n, g[n]["tdi"]))
+    
+        for nn in range(0, n):
+            local_diff = (g[n]["tdi"] ^ g[nn]["tdi"])
+            diff = diff | local_diff
+            #print("%d, %d: %x: %x, %x" % (n, nn, local_diff, g[n]["tdi"], g[nn]["tdi"]))
+
+    print("   %x, %c" % (diff, chr( (g[0]["tdi"] >> 3) & 0xff)))
 
 
 
---
+++ run.sh
@@ -0,0 +1,4 @@
+
+#./jtag_process.py ./traces/jtag_uart_i0_short.txt
+./jtag_process.py ./traces/jtag_uart_i2.txt
+
