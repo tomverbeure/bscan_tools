515ea894fe09985ad4d132d70ae2c92e8000774f
Tom Verbeure <hombre+github@gmail.com>
2020-07-17 23:02:03 -0700

    JTAG UART start.

---
+++ jtag_process.py
@@ -0,0 +1,153 @@
+#! /usr/bin/env python3
+
+import fileinput
+import sys
+import getopt
+
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+def usage():
+    sys.stderr.write("TODO...")
+    sys.stderr.write("\n")
+
+try:
+    opts, argv = getopt.getopt(sys.argv[1:], "r:", ["rename="])
+except getopt.GetoptError as err:
+    print(err)
+    usage()
+    sys.exit(2)
+
+rename_filename = None
+
+for o, a in opts:
+    if o in ("-r", "--rename"):
+        rename_filename = a
+
+if len(argv) != 1:
+    usage()
+    sys.exit(2)
+
+
+TEST_LOGIC_RESET    = 0
+RUN_TEST_IDLE       = 1
+SELECT_DR_SCAN      = 2
+CAPTURE_DR          = 3
+SHIFT_DR            = 4
+EXIT1_DR            = 5
+PAUSE_DR            = 6
+EXIT2_DR            = 7
+UPDATE_DR           = 8
+SELECT_IR_SCAN      = 9
+CAPTURE_IR          = 10
+SHIFT_IR            = 11
+EXIT1_IR            = 12
+PAUSE_IR            = 13
+EXIT2_IR            = 14
+UPDATE_IR           = 15
+
+def convert_tdi_tdo_str(str):
+
+    # Short format: 0x155
+    # Long format:  [0x0FECC0FF020B10DD], [0xFFFFFFFFFFFFFFFF], [0x1]
+
+    val = 0
+
+    str_segments = str.split(",")
+    if len(str_segments) == 1:
+        val = int(str_segments[0][2:], 16)
+    else: 
+        val = 0
+        start_bit = 0
+        for segment in str_segments:
+            val += int(segment.strip()[3:-1], 16) << start_bit
+            start_bit += 32
+
+    return val
+
+def read_transactions(jtag_filename):
+    transactions = []
+
+    state_lookup = {
+            "Test-Logic-Reset"  :    TEST_LOGIC_RESET,
+            "Run-Test/Idle"     :    RUN_TEST_IDLE,
+            "Select-DR-Scan"    :    SELECT_DR_SCAN,
+            "Capture-DR"        :    CAPTURE_DR,
+            "Shift-DR"          :    SHIFT_DR,
+            "Exit1-DR"          :    EXIT1_DR,
+            "Pause-DR"          :    PAUSE_DR,
+            "Exit2-DR"          :    EXIT2_DR,
+            "Update-DR"         :    UPDATE_DR,
+            "Select-IR-Scan"    :    SELECT_IR_SCAN,
+            "Capture-IR"        :    CAPTURE_IR,
+            "Shift-IR"          :    SHIFT_IR,
+            "Exit1-IR"          :    EXIT1_IR,
+            "Pause-IR"          :    PAUSE_IR,
+            "Exit2-IR"          :    EXIT2_IR,
+            "Update-IR"         :    UPDATE_IR,
+    }
+
+    with open(jtag_filename) as jtag_file:
+        first_line = True
+
+        for line in jtag_file:
+            if first_line:
+                first_line = False
+                continue 
+
+            time, tap_state, tdi, tdo, tdi_bits, tdo_bits = line.strip().split(";")
+
+            time = float(time)
+
+            if tap_state in state_lookup:
+                tap_state_nr    = state_lookup[tap_state]
+
+            if tdi_bits != "":
+                tdi_bits = int(tdi_bits)
+            else:
+                tdi_bits = 0
+
+            if tdo_bits != "":
+                tdo_bits = int(tdo_bits)
+            else:
+                tdo_bits = 0
+
+            if tdi != "": 
+                tdi = convert_tdi_tdo_str(tdi)
+                tdi = hex(tdi)
+            else:
+                tdi = None
+
+            if tdo != "": 
+                tdo = convert_tdi_tdo_str(tdo)
+                tdo = hex(tdo)
+            else:
+                tdo = None
+
+            #print(time, tap_state_nr, tap_state, tdi_bits, tdi, tdo_bits, tdo)
+
+            transaction = {
+                    "time"          :       time,
+                    "tap_state_nr"  :       tap_state_nr,
+                    "tap_state"     :       tap_state,
+                    "tdi_bits"      :       tdi_bits,
+                    "tdo_bits"      :       tdo_bits,
+                    "tdi"           :       tdi,
+                    "tdo"           :       tdo,
+                    }
+
+            transactions.append(transaction)
+
+    return transactions
+
+
+transactions = read_transactions(argv[0])
+
+pp.pprint(transactions)
+
+
+
+
+
---
+++ src/top.v
@@ -0,0 +1,39 @@
+
+
+module top(input clk);
+
+
+    reg reset_ = 1'b0;
+
+    always @(posedge clk) begin
+        reset_ <= 1'b1;
+    end
+
+    jtag_uart u_jtag_uart (
+		.clk_clk(clk),
+		.reset_reset_n(reset),
+		.avbus_w8_r64_chipselect(1'b0),   //  avbus_w8_r64.chipselect
+//		input  wire        avbus_w8_r64_address,      //              .address
+//		input  wire        avbus_w8_r64_read_n,       //              .read_n
+//		output wire [31:0] avbus_w8_r64_readdata,     //              .readdata
+//		input  wire        avbus_w8_r64_write_n,      //              .write_n
+//		input  wire [31:0] avbus_w8_r64_writedata,    //              .writedata
+//		output wire        avbus_w8_r64_waitrequest,  //              .waitrequest
+		.avbus_w16_r32_chipselect(1'b0),  // avbus_w16_r32.chipselect
+//		input  wire        avbus_w16_r32_address,     //              .address
+//		input  wire        avbus_w16_r32_read_n,      //              .read_n
+//		output wire [31:0] avbus_w16_r32_readdata,    //              .readdata
+//		input  wire        avbus_w16_r32_write_n,     //              .write_n
+//		input  wire [31:0] avbus_w16_r32_writedata,   //              .writedata
+//		output wire        avbus_w16_r32_waitrequest, //              .waitrequest
+		.avbus_w32r16_chipselect(1'b0)   //  avbus_w32r16.chipselect
+//		input  wire        avbus_w32r16_address,      //              .address
+//		input  wire        avbus_w32r16_read_n,       //              .read_n
+//		output wire [31:0] avbus_w32r16_readdata,     //              .readdata
+//		input  wire        avbus_w32r16_write_n,      //              .write_n
+//		input  wire [31:0] avbus_w32r16_writedata,    //              .writedata
+//		output wire        avbus_w32r16_waitrequest   //              .waitrequest
+	);
+
+endmodule
+
