15b00dadb00dc7170f913564f914c4d755a6c05b
Tom Verbeure <hombre+github@gmail.com>
2020-07-19 22:24:18 -0700

    ju4

---
+++ ju_rev_enc/ju4/traces/run.sh
@@ -0,0 +1,6 @@
+
+../../../jtag_process.py jtag_uart_i0.txt > jtag_uart_i0.log
+../../../jtag_process.py jtag_uart_i1.txt > jtag_uart_i1.log
+../../../jtag_process.py jtag_uart_i2.txt > jtag_uart_i2.log
+../../../jtag_process.py jtag_uart_i3.txt > jtag_uart_i3.log
+
